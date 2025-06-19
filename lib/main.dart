5c7bd998b07d7b28c82fd69e2ff7e356f6f45f38
Lenovo <samy.elmenshawy96@gmail.com>
2025-06-19 19:43:15 +0300

    - initialize project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c15/home_screen.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (_) => HomeScreen()},
+    );
+  }
+}
